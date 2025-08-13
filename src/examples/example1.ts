c82a5f630552cd03db92288dc63040a5421c8100
Maddi O’Toole <maddisan.otoole@gmail.com>
2025-08-14 08:27:23 +0930

    some changes

---
+++ src/examples/example1.ts
@@ -0,0 +1,11 @@
+function someLogic() {
+  console.log("some log");
+}
+
+function moreLogic() {
+  console.log("another log");
+  console.log("yet another");
+  console.log("commit");
+  // blah
+  // blah /blah
+}
